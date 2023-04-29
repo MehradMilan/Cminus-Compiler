e618800325906d2b2274deea02bf798a0046f02a
Mehrad <mehrad_milanloo@yahoo.com>
2023-04-29 16:52:51 +0330

    Add grammar to parser alphabet

---
+++ Parser.py
@@ -0,0 +1,39 @@
+import grammar
+
+class Lexim:
+
+    def __init__(self, name, is_terminal) -> None:
+        self.name = name
+        self.is_terminal = is_terminal
+
+class Alphabet:
+
+    def __init__(self, rules, first_sets, follow_sets, predict_sets) -> None:
+        self.lexims = []
+        self.rules = rules
+        self.first_sets = first_sets
+        self.follow_sets = follow_sets
+        self.predict_sets = predict_sets
+
+    def add_lexim(self, lexim):
+        self.lexims.append(lexim)
+
+    def extract_lexims_from_rules(self):
+        rules = self.rules
+        for rule in rules:
+            left = rule['left']
+            lexim = Lexim(left, False)
+            self.add_lexim(lexim)
+            for right in rule['right']:
+                if right not in self.lexims:
+                    lexim = Lexim(right, True)
+
+class Parser:
+
+    def __init__(self, grammar) -> None:
+        self.grammar = grammar
+        alphabet = Alphabet(self.grammar.rules, self.grammar.firsts_sets, self.grammar.follow_sets, self.grammar.predict_sets)
+        alphabet.extract_lexims_from_rules()
+        self.alphabet = alphabet
+
+parser = Parser(grammar)
