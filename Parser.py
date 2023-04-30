9627697c52a672f823144a5515c992ee24b35aa8
AliSafara <ali.safarafard@karnameh.ir>
2023-04-30 19:33:59 +0330

    Parsing Compeleted Without Panic-Mode

--- Parser.py
+++ Parser.py
@@ -1,10 +1,12 @@
-import grammar
+from anytree import Node, RenderTree
+
 
 class Lexim:
 
     def __init__(self, name, is_terminal) -> None:
         self.name = name
         self.is_terminal = is_terminal
+
 
 class Alphabet:
 
@@ -29,12 +31,42 @@ class Alphabet:
                     lexim = Lexim(right, True)
                     self.add_lexim(lexim)
 
-class Parser:
 
-    def __init__(self, grammar) -> None:
+class Parser:
+    def first_of(self, lexim):
+        if self.alphabet.lexims[lexim].is_terminal:
+            return [lexim]
+        return self.grammar.first_sets[lexim]
+
+    def follow_of(self, lexim):
+        if self.alphabet.lexims[lexim].is_terminal:
+            return []
+        return self.grammar.follow_sets[lexim]
+
+    def current_token_value(self):
+        return self.current_token[0] if self.current_token[0] in ['ID', 'NUM'] else self.current_token[1]
+
+    def __init__(self, grammar, first_token) -> None:
+        self.parse_tree = {0: 'Program'}
         self.grammar = grammar
-        alphabet = Alphabet(self.grammar.rules, self.grammar.first_sets, self.grammar.follow_sets, self.grammar.predict_sets)
+        alphabet = Alphabet(self.grammar.rules, self.grammar.first_sets, self.grammar.follow_sets,
+                            self.grammar.predict_sets)
         alphabet.extract_lexims_from_rules()
         self.alphabet = alphabet
+        self.current_token = first_token
 
-parser = Parser(grammar)
+    def parse(self, non_terminal, token_scanner, level, parent=None):
+        for lhs in self.grammar.rule_dict[non_terminal]:
+            if self.current_token_value() in self.first_of(lhs[0]) or \
+                    'epsilon' in self.first_of(lhs[0]) and self.current_token_value() in self.follow_of(lhs[0]):
+                for lexim in lhs:
+                    if self.alphabet.lexims[lexim].is_terminal:
+                        if lexim == self.current_token_value():
+                            Node('(' + self.current_token[0] + ', ' + self.current_token[1] + ')'
+                                 , parent=parent)
+                            self.current_token = next(token_scanner)
+                    else:
+                        self.parse(lexim, token_scanner, level + 1, Node(lexim, parent=parent))
+                break
+            elif 'epsilon' == lhs[0] and self.current_token_value() in self.follow_of(non_terminal):
+                Node('epsilon', parent=parent)
