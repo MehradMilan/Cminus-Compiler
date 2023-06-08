f05f1f7931e7ba02ec1c54f2a51370297f00bf01
Mehrad <mehrad_milanloo@yahoo.com>
2023-06-08 14:19:24 +0330

    Add data_block

--- Intermediate_Code_Generator/runtime_memory.py
+++ Intermediate_Code_Generator/runtime_memory.py
@@ -1,3 +1,5 @@
+INT_SIZE = 4
+
 class Memory:
     def __init__(self, pb_index, db_index, tb_index) -> None:
         self.PB = Program_Block(pb_index, db_index - 1)
@@ -6,9 +8,8 @@ class Program_Block:
     def __init__(self, base, bound) -> None:
         self.base = base
         self.bound = bound
-        self.length = bound - base + 1
-        self.current_index = 0
-        self.block = self.length * [None]
+        self.current_index = base
+        self.block = {}
 
     def add_instruction(self, instr=None, index=None):
         if instr == None:
@@ -34,3 +35,40 @@ class Instruction:
     
     def __str__(self) -> str:
         return f'({self.opcode}, {self.op1}, {self.op2}, {self.op3})'
+    
+class Temporary_Block:
+    def __init__(self, base, bound) -> None:
+        self.base = base
+        self.bound = bound
+        self.current_index = base
+        self.block = {}
+
+    def get_temp(self):
+        self.current_index += 1
+        # TODO
+  
+class Data:
+    def __init__(self, lexeme, type, address):
+        self.lexeme = lexeme
+        self.address = address
+        self.type = type
+        if type == 'int':
+            self.type_size = INT_SIZE
+        
+class Data_Block:
+    def __init__(self, base, bound) -> None:
+        self.base = base
+        self.bound = bound
+        self.current_index = base
+        self.block = {}
+
+    # def init_data_block(self): TODO
+
+    def create_data(self, lexeme, type):
+        data = Data(lexeme, type, self.current_index)
+        self.block[self.current_index] = data
+        self.increase_index(data.type_size)
+        return data
+
+    def increase_index(self, size):
+        self.current_index += size
